881d26c9963e812dea0ed65e838c1f4bf51a3813
pinhojuliana <studyjulianaa@gmail.com>
2024-09-24 18:39:16 -0300

    Resolução exercícios 11 a 19

--- src/main/java/ExerciciosApplication.java
+++ src/main/java/ExerciciosApplication.java
@@ -1,5 +1,26 @@
 public class ExerciciosApplication {
     public static void main(String[] args) {
+        PrincipaisMetodos principaisMetodos = new PrincipaisMetodos();
 
+
+        System.out.println("1- " + principaisMetodos.mostrarOrdemNumerica());
+        System.out.println("2- " + principaisMetodos.somarNumerosPares());
+        System.out.println("3- " + principaisMetodos.verificarSeSaoPositivos());
+        //principaisMetodos.removerValoresImpares();
+        System.out.println("5- " + principaisMetodos.calcularMediaNumerosMaioresCinco());
+        System.out.println("6- " + principaisMetodos.verificarMaiorQueDez());
+        System.out.println("7- " + principaisMetodos.encontrarSegundoMaiorNumero());
+        System.out.println("8- " + principaisMetodos.somarTodosOsNumeros());
+        System.out.println("9- " + principaisMetodos.verificarRepeticaoNumero());
+        System.out.println("10- " + principaisMetodos.agruparImparesMultTresCinco());
+        System.out.println("11- " + principaisMetodos.somarQuadrados());
+        System.out.println("12- " + principaisMetodos.multiplicarNumeros());
+        System.out.println("13- " + principaisMetodos.mostrarNumerosNoIntervalo(5,10));
+        System.out.println("14- " + principaisMetodos.encontrarMaiorPrimo());
+        System.out.println("15- " + principaisMetodos.verificarNegativo());
+        System.out.println("16- " + principaisMetodos.separarParImpar());
+        System.out.println("17- " + principaisMetodos.mostrarNumerosPrimos());
+        System.out.println("18- " + principaisMetodos.verificarSeSaoIguais());
+        System.out.println("19- " + principaisMetodos.somaNumerosDivisiveisTresCinco());
     }
 }
--- src/main/java/PrincipaisMetodos.java
+++ src/main/java/PrincipaisMetodos.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,59 +8,139 @@ public class PrincipaisMetodos {
     private List<Integer> numeros;
 
     public PrincipaisMetodos(){
-        this.numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 45);
+        this.numeros = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 45));
     }
 
-    //1
     public List<Integer> mostrarOrdemNumerica(){
         return numeros.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
-    //2
+
     public int somarNumerosPares(){
        return numeros.stream()
                .filter(n -> n%2 == 0 )
                .reduce(0, Integer::sum);
     }
-    //3
+
     public boolean verificarSeSaoPositivos(){
         return numeros.stream()
                 .allMatch(n -> n>0);
     }
-    //4
+
     public void removerValoresImpares(){
         numeros.removeIf(n -> n%2 != 0);
     }
-    //5
+
     public double calcularMediaNumerosMaioresCinco(){
         return numeros.stream().filter(n -> n>5)
                 .mapToDouble(n -> n)
                 .average().orElse(0.0);
     }
-    //6
+
     public boolean verificarMaiorQueDez(){
         return numeros.stream()
                 .anyMatch(n -> n>10);
     }
-    //7
-    public int encontrarMaiorNumero(){
-        return numeros.stream().max(Integer::compareTo).orElseThrow();
+
+    public int encontrarSegundoMaiorNumero(){
+        //return numeros.stream().max(Integer::compareTo).orElseThrow();
+        return numeros.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1) //ignora o maior numero
+                .findFirst() // pega o primeiro após o skip (segundo número)
+                .orElseThrow();
     }
-    //8
+
     public int somarTodosOsNumeros(){
         return numeros.stream().reduce(0, Integer::sum);
     }
-    //9
+
     public boolean verificarRepeticaoNumero(){
         return numeros.stream()
                 .distinct()
                 .count() != numeros.size();
     }
-    //10
+
     public List<Integer> agruparImparesMultTresCinco(){
         return numeros.stream().filter(n -> n%2!=0 && (n%3 == 0 || n%5 == 0))
                 .collect(Collectors.toList());
     }
 
+    public int somarQuadrados(){
+        return (int) numeros.stream()
+                .mapToDouble(n -> Math.pow(n, 2))
+                .sum();
+    }
+
+    public int multiplicarNumeros(){
+        return numeros.stream()
+                .reduce(1, (a,b) -> (a*b));
+    }
+
+    public List<Integer> mostrarNumerosNoIntervalo(int primeiro, int ultimo){
+        return numeros.stream()
+                .filter(n -> (n>primeiro && n<ultimo))
+                .collect(Collectors.toList());
+    }
+
+    public int encontrarMaiorPrimo(){
+        return numeros.stream()
+                .filter(this::isPrimo)
+                .max(Integer::compareTo)
+                .orElseThrow();
+    }
+    private boolean isPrimo(int numero) {
+        if (numero < 2) return false;
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean verificarNegativo(){
+        return numeros.stream()
+                .anyMatch(n -> n < 0);
+    }
+
+    public String separarParImpar(){
+        String pares = numeros.stream()
+                .filter(n -> n%2 ==0)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "Números pares: [", "]"));
+        String impares = numeros.stream()
+                .filter(n -> n%2 !=0)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "\nNúmeros impares:[", "]"));
+
+        return pares.concat(impares);
+    }
+
+    public List<Integer> mostrarNumerosPrimos(){
+      return  numeros.stream()
+              .filter(this::isPrimo)
+              .collect(Collectors.toList());
+    }
+
+    public boolean verificarSeSaoIguais(){
+        return numeros.stream()
+                .allMatch(n -> n.equals(numeros.get(0)));
+    }
+
+    public int somaNumerosDivisiveisTresCinco(){
+        return numeros.stream()
+                .filter(n ->(n%3 == 0 && n%5 == 0))
+                .reduce(0, Integer::sum);
+    }
+
+    public List<Integer> getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(List<Integer> numeros) {
+        this.numeros = numeros;
+    }
 }
